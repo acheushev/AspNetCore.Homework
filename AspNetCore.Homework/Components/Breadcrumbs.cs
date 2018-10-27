c5dcf94bf1d9f32e479668ab9f2b180c73084d1d
acheushev <alexander.cheushev@gmail.com>
2018-10-27 16:01:40 +0300

    Develop (#6)
    * Task 3.1. Layouts and Sections
    
    * Task 3.2. ViewComponents / PartialViews
    
    * Task 3.3 Html Helpers / Html Tags (Tag Helpers)

---
+++ AspNetCore.Homework/Components/Breadcrumbs.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Homework.Components
+{
+    public class Breadcrumbs : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var controller = ViewContext.RouteData.Values["Controller"];
+            var action = ViewContext.RouteData.Values["Action"];
+
+            if (controller.ToString().ToLower() == "home")
+                return View(model: (string)null);
+            if (action.ToString().ToLower() == "index")
+                return View(model: $"Home --> {controller}");
+            else
+                return View(model: $"Home --> {controller} --> {action}");
+        }
+    }
+}
---
+++ AspNetCore.Homework/Helpers/CategoryImageTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCore.Homework.Helpers
+{
+    [HtmlTargetElement("image",Attributes = "image-id")]
+    public class CategoryImageTagHelper:TagHelper
+    {
+        public int ImageId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var address = "/images/" + ImageId;
+            output.Attributes.SetAttribute("href", address);
+        }
+    }
+}
