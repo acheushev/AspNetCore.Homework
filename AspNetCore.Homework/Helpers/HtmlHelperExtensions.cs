7e37f5135ac718a0f319d50449c4a3c8c88f9c9b
Alexandr Cheushev <acheushev@vertafore.com>
2018-10-27 15:59:46 +0300

    Task 3.3 Html Helpers / Html Tags (Tag Helpers)

---
+++ AspNetCore.Homework/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace AspNetCore.Homework.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static HtmlString Image(this IHtmlHelper htmlHelper, int imageId, string text)
+        {
+            var a = new TagBuilder("a");
+
+            a.MergeAttribute("href", "/images/" + imageId);
+            a.InnerHtml.Append(text);
+            using (var stringWriter=new StringWriter())
+            {
+                a.WriteTo(stringWriter, HtmlEncoder.Default);
+                return new HtmlString(stringWriter.ToString());
+            }            
+        }
+    }
+}
