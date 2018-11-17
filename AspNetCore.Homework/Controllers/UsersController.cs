dfcd0c3fb63797cebbd180981d975226c5ca8f08
Alexandr Cheushev <acheushev@vertafore.com>
2018-11-17 20:39:55 +0300

    Task 5: Security

---
+++ AspNetCore.Homework/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCore.Homework.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.Homework.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserIdentityDbContext _userIdentityDbContext;
+
+        public UsersController(UserIdentityDbContext userIdentityDbContext)
+        {
+            _userIdentityDbContext = userIdentityDbContext;
+        }
+        // GET
+        public IActionResult Index()
+        {
+            return  View(_userIdentityDbContext.Users);
+        }
+    }
+}
---
+++ AspNetCore.Homework/Data/UserIdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore.Homework.Data
+{
+    public class UserIdentityDbContext : IdentityDbContext
+    {
+        public UserIdentityDbContext(DbContextOptions<UserIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
